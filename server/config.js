ada72248000ed413efa16f5e5e9b9f54ff766ea8
Alvaro Quero <aquerorodriguez@gmail.com>
2016-11-05 18:13:46 +0100

    Added basic database conection and User schema definition

---
+++ server/config.js
@@ -0,0 +1,5 @@
+export const db = { // eslint-disable-line 
+  host: 'localhost',
+  port: 28015,
+  db: 'expertsdb',
+};
---
+++ server/src/db/user.js
@@ -0,0 +1,9 @@
+// our packages
+import {thinky} from './thinky';
+
+export const User = thinky.createModel('User', {
+  name: thinky.type.string().required(),
+  email: thinky.type.string().required(),
+  password: thinky.type.string().required(),
+  registrationDate: thinky.type.date().default(thinky.r.now()),
+});
