359486fe5564162d665b849757a0a5404edd1861
Alvaro Quero <aquerorodriguez@gmail.com>
2016-11-07 21:41:03 +0100

    added basic auth methods

--- server/config.js
+++ server/config.js
@@ -1,5 +1,10 @@
-export const db = { // eslint-disable-line 
+export const db = { // eslint-disable-line
   host: 'localhost',
   port: 28015,
   db: 'expertsdb',
+};
+
+export const auth = {
+  passwordSalt: 'ex8Qf63vlzRJ3qP4a2dRFhLAh3VJ29Fq2EhuyT0WUEHRzYJyQLhBvYVBzU6vAzrZ',
+  sessionSecret: 'Tq9lf9dJ6dXZpZAL4MPuXg49HbXzwd5cc1nXTubQlQtRFO0A6noI8KSRwK8sCSx2',
 };
--- server/src/db/user.js
+++ server/src/db/user.js
@@ -2,8 +2,8 @@
 import {thinky} from './thinky';
 
 export const User = thinky.createModel('User', {
-  name: thinky.type.string().required(),
-  email: thinky.type.string().required(),
+  login: thinky.type.string().required(),
+  email: thinky.type.string(),
   password: thinky.type.string().required(),
   registrationDate: thinky.type.date().default(thinky.r.now()),
 });
