b10ec7d539a11038ec38e3301c6526e8670a4de3
Alvaro Quero <aquerorodriguez@gmail.com>
2016-11-08 18:40:29 +0100

    add login and register routes

--- server/src/auth/login.js
+++ server/src/auth/login.js
@@ -1,24 +1,8 @@
-// our packages
-import {User} from '../db';
-import {hash, asyncRequest} from '../util';
+// npm packages
+import passport from 'passport';
 
 export default (app) => {
-  app.post('/api/login', asyncRequest(async (req, res) => {
-    // get user input
-    const {login, password, passwordRepeat} = req.body;
-
-    if (password !== passwordRepeat) {
-      res.status(400).send({error: 'passwords do not match!'});
-      return;
-    }
-    const hashedPassword = hash(password);
-
-    const user = new User({
-      login,
-      password: hashedPassword,
-    });
-    await user.save();
-
-    res.sendStatus(201);
-  }));
+  app.post('/api/login', passport.authenticate('local'), (req, res) => {
+    res.send(req.user);
+  });
 };
