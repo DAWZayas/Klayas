603f4057e412708fba5333b92c3b89314354bfab
Alvaro Quero <aquerorodriguez@gmail.com>
2016-11-09 18:24:20 +0100

    Send JWT back after login

--- server/config.js
+++ server/config.js
@@ -7,4 +7,5 @@ export const db = { // eslint-disable-line
 export const auth = {
   passwordSalt: 'ex8Qf63vlzRJ3qP4a2dRFhLAh3VJ29Fq2EhuyT0WUEHRzYJyQLhBvYVBzU6vAzrZ',
   sessionSecret: 'Tq9lf9dJ6dXZpZAL4MPuXg49HbXzwd5cc1nXTubQlQtRFO0A6noI8KSRwK8sCSx2',
+  jwtSecret: 'KzoIqADNt90gEVyt8vzw50kYojBKJcUwgYzNRFIrGZZ35t9EgkVhEkb9stJDNRRn',
 };
