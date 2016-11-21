ed4dc60bfbe124e3dd3e94e89982cf5f9990cdab
Alvaro Quero <aquerorodriguez@gmail.com>
2016-11-21 19:13:43 +0100

    Set up basic client project with webpack, babel and react

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (
+  <h1>Hello, world!</h1>
+);
+
+// render on page
+ReactDOM.render (App, document.getElementById('app'));
