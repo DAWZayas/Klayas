3b25fd39a82bfa5f70f5cdc8cd1234fe2eb11dd7
Alvaro Quero <aquerorodriguez@gmail.com>
2016-11-23 20:43:30 +0100

    Setup basic routing using react-router@4

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
+// npm packages
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-  <h1>Hello, world!</h1>
-);
+// our packages
+import App from './app';
 
 // render on page
 ReactDOM.render (<App />, document.getElementById('app'));
