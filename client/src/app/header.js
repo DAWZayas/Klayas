d7ddbf312c221942bf6f9b63ef5a81dfd6862efb
Juan Medina <soyjuanmedina@gmail.com>
2016-11-27 19:41:15 +0100

    Add header to the app

---
+++ client/src/app/header.js
@@ -0,0 +1,11 @@
+// npm packages
+import React from 'react';
+
+const Header = () => (
+  <div class="page-header">
+  <h1>Klayas <small>Las clases del Zayas</small></h1>
+  <img src={require('./images/logo.png')} />
+</div>
+);
+
+export default (Header);
--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,8 +1,13 @@
 // npm packages
 import React from 'react';
 
+//our components
+import Header from './header.js';
+
 export default ({children}) => (
   <div className="container">
+
+  <Header />
     {children}
   </div>
 );
