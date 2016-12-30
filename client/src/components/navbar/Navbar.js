a77a2b017ab7eec3a502145d8142eff6557ef68e
Alvaro Quero <aquerorodriguez@gmail.com>
2016-12-30 17:21:03 +0100

    [client] Changed the navbar and the App container to redirect th user is logged in

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,41 +1,75 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {IndexLink} from 'react-router';
+import {Button} from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {push} from 'react-router-redux';
+import {logoutAction} from '../../store/actions';
 
 const styles = require('./Navbar.scss');
 
-export default () => (
-  <nav className="navbar navbar-default">
-    <div className="container-fluid">
-      {/* Brand and toggle get grouped for better mobile display */}
-      <div className="navbar-header">
-        <button
-          type="button"
-          className="navbar-toggle collapsed"
-          data-toggle="collapse" data-target="#navbar-collapse-1"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
-        <IndexLink to="/" className={`navbar-brand ${styles.navbar}`}>
-          <span>Klayas</span>
-        </IndexLink>
-      </div>
+const mapDispatchToProps = dispatch => ({
+  onLoginClick: () => dispatch(push('/login')),
+  onLogoutClick: () => dispatch(logoutAction()),
+});
 
-      <div className="collapse navbar-collapse" id="navbar-collapse-1">
-        <ul className="nav navbar-nav navbar-right">
-          <li>
-            <button
-              type="button"
-              className={`btn btn-primary ${styles.button}`}
-            >
-              <span className="glyphicon glyphicon-log-in" /> Inicia sesi&oacute;n
-            </button>
-          </li>
-        </ul>
+const Navbar = ({onLoginClick, onLogoutClick, token}) => {
+  const handleLoginClick = (e) => {
+    e.preventDefault();
+    onLoginClick();
+  };
+
+  const handleLogoutClick = (e) => {
+    e.preventDefault();
+    onLogoutClick();
+  };
+
+  return (
+    <nav className="navbar navbar-default navbar-static-top">
+      <div className="container-fluid">
+        {/* Brand and toggle get grouped for better mobile display */}
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse" data-target="#navbar-collapse-1"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <IndexLink to="/" className={`navbar-brand ${styles.navbar}`}>
+            <span>Klayas</span>
+          </IndexLink>
+        </div>
+
+        <div className="collapse navbar-collapse" id="navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right">
+            {!token ? (
+              <li>
+                <Button bsStyle="primary" bsClass={styles.button} onClick={handleLoginClick}>
+                  <span className="glyphicon glyphicon-log-in" /> Inicia sesi&oacute;n
+                </Button>
+              </li>
+            ) : (
+              <li>
+                <Button bsStyle="primary" bsClass={styles.button} onClick={handleLogoutClick}>
+                  <span className="glyphicon glyphicon-log-out" /> Cerrar sesi&oacute;n
+                </Button>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  onLoginClick: PropTypes.func.isRequired,
+  onLogoutClick: PropTypes.func.isRequired,
+  token: PropTypes.string,
+};
+
+export default connect(null, mapDispatchToProps)(Navbar);
--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -1,12 +1,53 @@
 // npm packages
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {push} from 'react-router-redux';
 
 // our components
 import {Navbar} from '../../components';
 
-export default ({children}) => (
-  <div className="container-fluid">
-    <Navbar />
-    {children}
-  </div>
-);
+const mapStateToProps = state => ({
+  actualPath: state.routing.locationBeforeTransitions && state.routing.locationBeforeTransitions.pathname,
+  user: state.auth.user,
+  token: state.auth.token,
+});
+
+const mapDispatchToProps = dispatch => ({
+  navTo: path => dispatch(push(path)),
+});
+
+class App extends React.Component {
+  static propTypes = {
+    token: PropTypes.string,
+    user: PropTypes.shape({
+      email: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      login: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      registrationDate: PropTypes.string.isRequired,
+      surname: PropTypes.string.isRequired,
+    }),
+    navTo: PropTypes.func.isRequired,
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.token !== nextProps.token) {
+      if (nextProps.token) {
+        return this.props.navTo('/');
+      }
+      return this.props.navTo('/home');
+    }
+  }
+
+  render() {
+    const {children, user, token} = this.props;
+    return (
+      <div>
+        <Navbar {...this.props} />
+        {children}
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
