37cc743bffca02c153aceeff9f4574e4019ed4a8
Alvaro Quero <aquerorodriguez@gmail.com>
2017-01-03 19:33:24 +0100

    [client] Added FA-icons support, buttons to social login and action to login

--- client/src/components/loginModal/LoginModal.js
+++ client/src/components/loginModal/LoginModal.js
@@ -1,47 +1,82 @@
-import React from 'react';
-import {Modal, Button, Form, FormGroup, Col, FormControl, Checkbox} from 'react-bootstrap';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {Modal, Button} from 'react-bootstrap';
+
+// our packages
+import {loginAction} from '../../store/actions';
 
 const styles = require('./LoginModal.scss');
 
-export default ({show, close}) => (
-  <Modal show={show} onHide={close}>
-    <Modal.Header closeButton>
-      <h4>Inicia sesi&oacute;n en tu cuenta</h4>
-    </Modal.Header>
-    <Modal.Body>
-      <div className={`row ${styles.rowDivided}`}>
-        <div className="col-xs-6">
-          <form>
-            <div className="form-group has-feedback">
-              <input type="text" className="form-control" placeholder="Username" />
-              <i className="glyphicon glyphicon-user form-control-feedback" />
-            </div>
-            <div className="form-group has-feedback">
-              <input type="password" className="form-control" placeholder="Password" />
-              <i className="glyphicon glyphicon-lock form-control-feedback" />
-            </div>
-            <div>
-              <label htmlFor="remember_me">
-                <input type="checkbox" value="1" name="remember_me" checked="checked" />
-                <span > Recordar mis datos</span>
-              </label>
-              <span className="separator"> · </span>
-              <a>¿Olvidaste tu contraseña?</a>
-            </div>
-            <Button bsStyle="primary" block>Iniciar sesi&oacute;n</Button>
-          </form>
+const mapDispatchToProps = dispatch => ({
+  onLoginClick: params => dispatch(loginAction(params)),
+});
+
+
+const LoginModal = ({onLoginClick, show, close}) => {
+  LoginModal.propTypes = {
+    onLoginClick: PropTypes.func,
+    show: PropTypes.bool,
+    close: PropTypes.func,
+  };
+
+  let usernameInput;
+  let passwordInput;
+  let rememberInput;
+
+  const handleLoginClick = (e) => {
+    e.preventDefault();
+
+    onLoginClick({
+      login: usernameInput.value,
+      password: passwordInput.value,
+      remember: rememberInput.checked,
+    });
+    close();
+  };
+
+  return (
+    <Modal show={show} onHide={close}>
+      <Modal.Header closeButton>
+        <h4>Inicia sesi&oacute;n en tu cuenta</h4>
+      </Modal.Header>
+      <Modal.Body>
+        <div className={`row ${styles.rowDivided}`}>
+          <div className="col-xs-6">
+            <form>
+              <div className="form-group has-feedback">
+                <input type="text" className="form-control" placeholder="Username" ref={(i) => { usernameInput = i; }} />
+                <i className="glyphicon glyphicon-user form-control-feedback" />
+              </div>
+              <div className="form-group has-feedback">
+                <input type="password" className="form-control" placeholder="Password" ref={(i) => { passwordInput = i; }} />
+                <i className="glyphicon glyphicon-lock form-control-feedback" />
+              </div>
+              <div>
+                <label htmlFor="remember_me">
+                  <input type="checkbox" value="1" name="remember_me" ref={(i) => { rememberInput = i; }} />
+                  <span > Recordar mis datos</span>
+                </label>
+                <span className="separator"> · </span>
+                <a>¿Olvidaste tu contraseña?</a>
+              </div>
+              <Button type="submit" bsStyle="primary" block onClick={handleLoginClick}>Iniciar sesi&oacute;n</Button>
+            </form>
+          </div>
+          <div className={styles.verticalDivider}>
+            <p>o</p>
+          </div>
+          <div className={`col-xs-5 col-xs-offset-1 ${styles.verticalAlign}`}>
+            <Button bsStyle="primary" className={styles.btnGoogle} block>
+              <i className="fa fa-google fa-lg social" aria-hidden="true" /> Iniciar sesi&oacute;n con Google
+            </Button>
+            <Button bsStyle="primary" className={styles.btnGithub} block>
+              <i className="fa fa-github fa-lg social" aria-hidden="true" /> Iniciar sesi&oacute;n con Github
+            </Button>
+          </div>
         </div>
-        <div className={`col-xs-1 ${styles.verticalDivider}`}>
-          <p>o</p>
-        </div>
-        <div className="col-xs-5">
-          <Button bsStyle="link" block>
-            <span className="" />
-            Iniciar sesi&oacute;n con Google
-          </Button>
-          <Button bsStyle="link" block>Iniciar sesi&oacute;n con Github</Button>
-        </div>
-      </div>
-    </Modal.Body>
-  </Modal>
-);
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default connect(null, mapDispatchToProps)(LoginModal);
