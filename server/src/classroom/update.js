3454e4c65fe99b7e69f6c48a2f5eb739197d10d0
Alvaro <aquerorodriguez@gmail.com>
2017-01-18 13:37:30 +0100

    [client & server] Fix an error with the date when you try to update classroom and allow us to modify url with empty data

--- server/src/classroom/update.js
+++ server/src/classroom/update.js
@@ -42,9 +42,7 @@ export default (app) => {
       classroom.description = description;
     }
 
-    if (url) {
-      classroom.url = url;
-    }
+    classroom.url = url;
 
     // if (isPublic) {
     //   classroom.isPublic = isPublic;
