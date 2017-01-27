f19da2cb55e543c0d7e54c29d6f7d9ca35c40d5b
Juan Medina <soyjuanmedina@gmail.com>
2017-01-27 20:52:11 +0100

    [In progress] Pagination

--- client/src/components/classroom/ClassroomList.js
+++ client/src/components/classroom/ClassroomList.js
@@ -18,31 +18,51 @@ class ClassroomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      questionIndex: 0,
+      classroomIndex: 0,
     };
+
   }
 
   componentDidMount() {
     const {classrooms} = this.props;
+
+
     // loadMore({
     //   skip: classrooms.length,
     //   limit: 10,
     // });
   }
     render() {
-       const {classrooms, user, hasMore, questionIndex} = this.props;
+       const {classrooms, user, hasMore} = this.props;
+       const {classroomIndex} = this.state;
+
+       const classroom = classrooms[classroomIndex];
+       console.log(classroom);
+
+       const handleClick = (inc) => {
+
+      this.setState({
+        classroomIndex: classroomIndex + inc,
+      });
+
+      return false;
+    };
   // const ClassroomList = ({classrooms, user, hasMore}) => {
     return (
       <div>
-        {classrooms.map((classroom, index) => (
-          classroom.teacher === user.id ? <ClassroomOwner key={index} classroom={classroom} /> : null
-        ))}
-        <div className="btn-group col-xs-4 col-xs-offset-5" role="group">
-          <button type="button" className="btn btn-default" disabled={questionIndex === 0} onClick={() => handleClick(-1)}>
+
+        {!hasMore && classrooms.length === 0 ? <div>No questions yet!</div> : null}
+        {classroomIndex > classrooms.length ? "hola" : classroom ? <ClassroomOwner onLoad={() => handleClick(+1)} key={classroom.id} classroom={classroom} /> : classrooms.length > 0 ? 'No more questions' : null}
+         <div className="btn-group col-xs-4 col-xs-offset-5" role="group">
+          <button type="button"
+            className="btn btn-default"
+            disabled={classroomIndex === 0}
+            onClick={() => handleClick(-1)}>
             <span className="glyphicon glyphicon-arrow-left" />
           </button>
           <button type="button"
             className="btn btn-default"
+            disabled={classroomIndex === classrooms.length - 1}
             onClick={() => handleClick(+1)}>
             <span className="glyphicon glyphicon-arrow-right" />
           </button>
