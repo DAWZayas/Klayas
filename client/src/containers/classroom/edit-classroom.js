c456dc12e70727dd8e7da22288d7a0802bdc7255
Alvaro Quero <aquerorodriguez@gmail.com>
2017-02-18 13:31:34 +0100

    [client] Finish the edit-classroom page

--- client/src/containers/classroom/edit-classroom.js
+++ client/src/containers/classroom/edit-classroom.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import moment from 'moment';
 import {push} from 'react-router-redux';
-import {Card} from 'material-ui';
+import {Card, RaisedButton, TextField} from 'material-ui';
 
 // our packages
 import {updateClassAction} from '../../store/actions';
@@ -21,92 +21,46 @@ const mapDispatchToProps = dispatch => ({
 const EditClassroom = ({onCreateClick, navToClassroom, classroom, error}) => {
   let nameInput;
   let descriptionInput;
-  let urlInput;
-  let dateInput;
-  let timeInput;
-  let publicInput;
 
   const handleClick = (e) => {
     e.preventDefault();
-    setImmediate(() => navToClassroom());
     onCreateClick({
-      name: nameInput.value,
-      description: descriptionInput.value,
-      url: urlInput.value,
-      date: moment(dateInput.value).toISOString(),
-      time: timeInput.value,
+      name: nameInput.getValue(),
+      description: descriptionInput.getValue(),
       id: classroom.id,
     });
+    navToClassroom();
   };
-
-  const d = moment(classroom.date).format('YYYY-MM-DD');
 
   return (
     <Card className="containerPaper">
-      <h2>Klayas:</h2>
       <p>{"Edit your Klayas' classroom"}</p>
       <form>
-        <div className="form-group">
-          <label htmlFor="inputName">Classroom name:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputName"
-            defaultValue={classroom.name}
-            ref={(i) => { nameInput = i; }}
-          />
+        <div className="row">
+          <div className="col-xs-12">
+            <TextField
+              hintText="Change your classroom name"
+              floatingLabelText="Classroom name"
+              defaultValue={classroom.name}
+              ref={(i) => { nameInput = i; }}
+            />
+          </div>
+          <div className="col-xs-12">
+            <TextField
+              hintText="Change the description"
+              floatingLabelText="Description"
+              ref={(i) => { descriptionInput = i; }}
+            />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="inputDescription">Description:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputDescription"
-            defaultValue={classroom.description}
-            ref={(i) => { descriptionInput = i; }}
-          />
+        <div className="row marTop1_5em">
+          <div className="col-xs-6">
+            <RaisedButton label="Update" primary onTouchTap={handleClick} />
+          </div>
+          <div className="col-xs-6">
+            <RaisedButton label="Cancel" backgroundColor="#C62828" labelColor="white" onTouchTap={navToClassroom} />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="inputUrl">Url:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputUrl"
-            defaultValue={classroom.url}
-            ref={(i) => { urlInput = i; }}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputDate">{'Date'}:</label>
-          <input
-            type="date"
-            className="form-control"
-            id="inputDate"
-            defaultValue={d}
-            ref={(i) => { dateInput = i; }}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="inputLogin">Hour:</label>
-          <input
-            type="time"
-            className="form-control"
-            id="inputTime"
-            defaultValue={classroom.time}
-            ref={(i) => { timeInput = i; }}
-          />
-        </div>
-        <div className="checkbox">
-          <label htmlFor="inputPublic">
-            <input
-              type="checkbox"
-              id="inputPublic"
-              ref={(i) => { publicInput = i; }}
-            /> {'Is public'}
-          </label>
-        </div>
-        <button type="submit" className="btn btn-default" onClick={handleClick}>Modificar Clase</button> &nbsp;| &nbsp;
-        <button type="submit" className="btn btn-default" onClick={navToClassroom}>Cancelar</button>
       </form>
     </Card>
   );
