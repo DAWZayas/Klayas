49e911c83dfb48e54699fe6b3b588030f8a3ba66
Juan Medina <soyjuanmedina@gmail.com>
2017-02-18 09:23:22 +0100

    The user LineChat float to right

--- client/src/components/classroom/LineChat.js
+++ client/src/components/classroom/LineChat.js
@@ -2,28 +2,47 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Avatar, Chip} from 'material-ui';
 
-// our packages
-import {getOneClassRoom, addObservable} from '../../store/actions';
-import {registerClassroomObservable} from '../../store/realtime';
-
 const mapStateToProps = (state, ownProps) => ({
   line: ownProps.line,
+  user: state.auth.user,
 });
 
-const LineChat = ({line}) => {
+const LineChat = ({line, user}) => {
+
+  const divStyleRight = {
+    float: 'rigth',
+    color: 'red',
+  };
 
   return (
     <div>
-      <Chip>
-        <Avatar src={line.studentavatar} />
-          <div style={{fontSize: 'smaller', color: 'red'}}>
-            {line.studentname}
-          </div>
-          <div>
-            {line.text}
-          </div>
-      </Chip>
-      <br/>
+    { user.id === line.studentid ? (
+      <div style={{float: 'right', clear: 'both'}} >
+        <Chip>
+          <Avatar src={line.studentavatar} />
+            <div style={{fontSize: 'smaller', color: 'red'}}>
+              You says
+            </div>
+            <div>
+              <p>{line.text}</p>
+            </div>
+        </Chip>
+        <br/>
+      </div>
+    ): (
+      <div>
+        <Chip>
+          <Avatar src={line.studentavatar} />
+            <div style={{fontSize: 'smaller', color: 'red'}}>
+              {line.studentname} says
+            </div>
+            <div>
+              <p>{line.text}</p>
+            </div>
+        </Chip>
+        <br/>
+      </div>
+    ) }
 
     </div>
   );
